9782af49026df79e19d0fb624f3097338b8428d6
Cory Spitzer <cory@coryspitzer.com>
2021-06-02 10:03:39 -0400

    Add test data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the 
+# database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or 
+# created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.destroy_all
+Issue.destroy_all
+Comment.destroy_all
+User.destroy_all
+
+Project.create!([{
+  title: "Proj1",
+  summary: "The 1st project",
+  description: "Create a good project using Rails"
+},
+{
+  title: "The issue tracker",
+  summary: "A basic issue tracker",
+  description: "A basic issue tracker made with Ruby, Rails, and SQLite3"
+  
+},
+{
+  title: "Take over the world",
+  summary: "A half-hearted attempt to take over the world",
+  description: "Take over the world using only Ruby, Rails, and SQLite3"
+  
+}])
