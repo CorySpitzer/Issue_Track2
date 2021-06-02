db73abdde77d1c1e89521011fd70f192893b4079
Cory Spitzer <cory@coryspitzer.com>
2021-06-02 11:31:27 -0400

    Add data with appropriate foreign key

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,7 @@
 class Issue < ApplicationRecord
     has_and_belongs_to_many :users
     has_many :comments
-    belongs_to :projects
+    belongs_to :projects, optional: true
     validates :summary, presence: true
     validates :description, presence: true
     validates :status, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,43 @@ Issue.destroy_all
 Comment.destroy_all
 User.destroy_all
 
-Project.create!([{
-  title: "Proj1",
-  summary: "The 1st project",
-  description: "Create a good project using Rails"
-},
-{
-  title: "The issue tracker",
-  summary: "A basic issue tracker",
-  description: "A basic issue tracker made with Ruby, Rails, and SQLite3"
+project0 = Project.create!(title: "Proj0",
+                           summary: "The 0th project", 
+                           description: "Create a good project using Rails")
+
+Issue.create!(summary: "issue summary0",
+              description: "description0",
+              status: "Incomplete",
+              project_id: project0.id)
+              
+# Project.create!([{
+# # projects = Project.create!([{
+# title: "Proj1",
+#   summary: "The 1st project",
+#   description: "Create a good project using Rails"
+# },
+# {
+#   title: "The issue tracker",
+#   summary: "A basic issue tracker",
+#   description: "A basic issue tracker made with Ruby, Rails, and SQLite3"
   
-},
-{
-  title: "Take over the world",
-  summary: "A half-hearted attempt to take over the world",
-  description: "Take over the world using only Ruby, Rails, and SQLite3"
+# },
+# {
+#   title: "Take over the world",
+#   summary: "A half-hearted attempt to take over the world",
+#   description: "Take over the world using only Ruby, Rails, and SQLite3"
   
-}])
+# }])
+
+# Issue.create!([{
+    # summary: "issue summary0",
+    # description: "description0",
+    # status: "Incomplete",
+    # project_id: projects[0]
+# },
+# {
+#     summary: "issue summary1",
+#     description: "description1",
+#     status: "Complete",
+#     project_id: projects[0]
+# }])
