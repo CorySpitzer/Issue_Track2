f7c4847a63f3e6a38b18b2f9ee7c34200cabd44e
Cory Spitzer <cory@coryspitzer.com>
2021-06-03 09:16:17 -0400

    Add more explanatory comments

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,5 @@
 class IssuesController < ApplicationController
   def index
-    <h2>Issues: </h2>
-    
+
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,14 @@
 class Issue < ApplicationRecord
     has_and_belongs_to_many :users
     has_many :comments
+
+    # belongs_to :projects
     belongs_to :projects, optional: true
+    # 'optional: true' prevents the error
+    # ActiveRecord::RecordInvalid: Validation failed: Projects must exist
+    # when running rake db:seed
+
+
     validates :summary, presence: true
     validates :description, presence: true
     validates :status, presence: true
