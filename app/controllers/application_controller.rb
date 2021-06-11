d3b8527e3e882cda0282d63decd0d8bb94df1aa5
Cory Spitzer <cory@coryspitzer.com>
2021-06-11 12:05:14 -0400

    Display email and role with partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
+    # all this is to allow the dropdown menu in registration:
     protect_from_forgery with: :exception
-
     before_action :configure_permitted_parameters, if: :devise_controller?
-
     protected
-
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
             devise_parameter_sanitizer.permit(:account_update, keys: [:role])
