63ec457f8a218fb491f0b1c61d4d2b29b0a85baa
Cory Spitzer <coryspitzer@gmail.com>
2021-06-19 09:36:18 -0400

    Delete projects (and their issues and comments)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,21 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def destroy
+    @project = Project.find(params[:id])
+    # We can't delete the project without deleting it's children and their children
+    # First delete the grandkids, comments
+    @project.issues.each do |issue|
+      issue.comments.each do |comment|
+        comment.destroy
+      end
+    end
+    # then delete the kids,issues
+    @project.issues.each do|issue|
+      issue.destroy
+    end
+    @project.destroy
+    redirect_back fallback_location: '/'
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
-    has_many :issues
+    has_many :issues#, :dependent => :destroy 
+    #this is no longer needed because we do it manually.
+    
     validates :summary, presence: true
     validates :description, presence: true
     validates :title, presence: true
