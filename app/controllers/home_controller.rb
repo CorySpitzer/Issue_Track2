a52a213b7edf79d2dbb19ef26379e06ecd935999
Cory Spitzer <coryspitzer@gmail.com>
2021-06-22 09:02:04 -0400

    Connect a user with an issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,12 @@ class HomeController < ApplicationController
     # Here we must associate a user with a issue through the
     # join table by adding the user id and issue id to it
     # user = User.find(params[:user_id])
-    # issue = Issue.find(params[issue.id])
-    a
+    # issue = Issue.find(params[:issue.id])
+    if User.ids.include?(params[:user_id].to_i) and Issue.ids.include?(params[:issue_id].to_i)
+      User.find(params[:user_id]).issues << Issue.find(params[:issue_id])
+    else
+      
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
