30b9d3d4a4b655a0ea7e7a9c740b3fadd5538e4b
Cory Spitzer <coryspitzer@gmail.com>
2021-06-28 10:23:48 -0400

    Link to individual users pages

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "/comments/:id", to: "comments#show"
   get "/project_reports", to: "project_reports#index"
 
-  resources :projects, :issues
+  resources :projects, :issues, :users
   post "projects/new", to: "projects#create"
   # "creates seven different routes in your application"
   # https://guides.rubyonrails.org/routing.html
