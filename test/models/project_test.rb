c8438917edb27f84365dcc2666677396f616ae41
Cory Spitzer <coryspitzer@gmail.com>
2021-07-08 11:47:22 -0400

    Test that Projects validate title

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,4 +4,8 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save without a title" do
+    project = Project.new(description: "Proj2", summary: "The 3rd proj")
+    assert_not project.save
+  end
 end
