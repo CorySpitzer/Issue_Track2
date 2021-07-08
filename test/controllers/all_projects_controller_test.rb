3fe7538534c1dd9e75174f9c12d4a80b0d9e8e0b
Cory Spitzer <coryspitzer@gmail.com>
2021-07-08 12:07:06 -0400

    Test that will not save Projects without description

--- test/controllers/all_projects_controller_test.rb
+++ test/controllers/all_projects_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class AllProjectsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get all_projects_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get all_projects_index_url
+  #   assert_response :success
+  # end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -8,4 +8,12 @@ class ProjectTest < ActiveSupport::TestCase
     project = Project.new(description: "Proj2", summary: "The 3rd proj")
     assert_not project.save
   end
+  test "should not save without a description" do
+    project = Project.new(title: "Proj2", summary: "The 3rd proj")
+    assert_not project.save
+  end
+  # test "should not save without a title" do
+  #   project = Project.new(description: "Proj2", summary: "The 3rd proj")
+  #   assert_not project.save
+  # end
 end
