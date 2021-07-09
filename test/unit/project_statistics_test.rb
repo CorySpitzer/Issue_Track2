523b0e7f698ee5b193d4611d0f1862597b763a44
Cory Spitzer <coryspitzer@gmail.com>
2021-07-09 09:28:44 -0400

    Try to test count_resolved_issues

--- test/unit/project_statistics_test.rb
+++ test/unit/project_statistics_test.rb
@@ -2,11 +2,27 @@ require "test_helper"
 require "calculators"
 
 class ProjectStatisticsTest < ActiveSupport::TestCase
-    test "" do
+    test "project statistics can be made" do
         proj = Project.create!(title: "Proj0",
                             summary: "The 0th project", 
                             description: "Create a good project using Rails",
                             status: "incomplete")
         proj_stats = ProjectStatistics.new proj.id
+        assert_instance_of(ProjectStatistics, proj_stats)
+    end
+
+    test "count resolved issues" do
+        proj = Project.create!(title: "Proj0",
+                            summary: "The 0th project", 
+                            description: "Create a good project using Rails",
+                            status: "incomplete")
+        issue0 = Issue.create!(summary: "issue summary0",
+                            description: "description0",
+                            status: "resolved",
+                            project_id: proj.id) 
+        proj_stats = ProjectStatistics.new proj.id
+        # p proj_stats.count_resolved_issues
+        resolved_issues = proj_stats.count_resolved_issues
+        assert_equal(1, resolved_issues)
     end
 end
