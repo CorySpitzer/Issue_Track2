799d2ae2fd7501e827b839f78158e7aa21b8c465
Cory Spitzer <coryspitzer@gmail.com>
2021-07-10 11:21:41 -0400

    Test counting resolved issues

--- spec/calculators_spec.rb
+++ spec/calculators_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 require_relative '../lib/calculators'
 require_relative '../app/models/project'
 
-describe ProjectStatistics do
-    it 'exists when created' do
-        project = Project.create!(title: "Proj0",
+project = Project.create!(title: "Proj0",
                                 summary: "The 0th project", 
                                 description: "Create a good project using Rails",
                                 status: "incomplete")
-        proj_stats = ProjectStatistics.new(project.id) 
+
+proj_stats = ProjectStatistics.new(project.id)
+
+issue = Issue.create!(summary: "issue summary3",
+              description: "description4",
+              status: "Resolved",
+              project_id: project.id) 
+
+describe ProjectStatistics do
+    it 'exists when created' do 
         expect(proj_stats).to be_an_instance_of(ProjectStatistics)
+    end
+
+    it 'counts resolved issues' do
+        expect(proj_stats.count_resolved_issues).to equal 1
     end
 end
