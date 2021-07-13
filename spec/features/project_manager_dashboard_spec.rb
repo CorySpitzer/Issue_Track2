7c36aad103ee35d2b1847cde00c1d13f45b73928
Cory Spitzer <coryspitzer@gmail.com>
2021-07-13 12:09:14 -0400

    Fail test to delete issues

--- spec/features/project_manager_dashboard_spec.rb
+++ spec/features/project_manager_dashboard_spec.rb
@@ -12,6 +12,11 @@ user2 = User.create!(username: "PM22",
             password: "GoHome4",
             password_confirmation: "GoHome4")
 
+issue1 = Issue.create!(summary: "issue summary22",
+            description: "description1",
+            status: "Resolved",
+            project_id: project22.id) 
+
 RSpec.describe 'the project manager dashboard', type: :feature do 
     scenario 'click on a project link' do
         # First log in the user
@@ -32,7 +37,18 @@ RSpec.describe 'the project manager dashboard', type: :feature do
         fill_in 'Password', with: user2.password
         click_on 'Log in'
         # Next, click on a project from the list
-        click_on 'Delete id ' + project22.id.to_s
+        click_on 'Delete project ' + project22.id.to_s
         expect(page).not_to have_content project22.title
+    end
+    scenario 'delete an issue' do
+        # First log in the user
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: user2.email
+        fill_in 'Password', with: user2.password
+        click_on 'Log in'
+        # Next, click on a project from the list
+        click_on 'Delete issue ' + issue1.id.to_s
+        expect(page).to have_content issue1.summary
     end
 end
