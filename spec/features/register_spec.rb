8f9a05a4da5e6cd54906dd38aa7fddbe8e6f7774
Cory Spitzer <coryspitzer@gmail.com>
2021-07-18 09:40:09 -0400

    Test the users path

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+# We want to test our custom code with register, not what devise provides,
+# which is well-tested
+# RSpec.describe 'the Register path', type: :feature do 
+#     scenario 'register an end user' do
+#         visit root_path
+#         click_on 'Register'
+#         fill_in 'Email', with: 'testing@testing.com'
+#         fill_in 'Password', with: 'NoOneCanGuessMyGreatPassword'
+#         select 'end_user', :from => 'role'
+#     end
+# end
+
+# error:
+#   Unable to find select box "role" that is not disabled
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,1 +1,25 @@
 require 'rails_helper'
+
+RSpec.describe 'the project manager dashboard', type: :feature do 
+
+    scenario 'click on a project link' do
+        # This needs to be in the scenario so it is rolled back in a transaction
+        # Put project managers in the DB:
+        user2 = User.create!(username: "PM22",
+                            role: "project_manager",
+                            email: "p1@d.com",
+                            password: "GoHome4",
+                            password_confirmation: "GoHome4")
+
+        # First log in the user
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: user2.email
+        fill_in 'Password', with: user2.password
+        click_on 'Log in'
+
+        # visit the users page, which is only available to a PM
+        visit users_path 
+        expect(page).to have_content user2.username
+    end
+end
