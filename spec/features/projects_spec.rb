63ee90abf96b8cea3c3f7bd0f3c0f1fbc08fe78f
Cory Spitzer <coryspitzer@gmail.com>
2021-08-08 05:56:02 -0400

    Test creating a new issue from a project page

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -18,3 +18,29 @@ RSpec.describe 'the projects page', type: :feature do
         expect(page).to have_content 'issue summary23'
     end
 end
+
+RSpec.describe 'the new issue path', type: :feature do 
+    scenario 'fill in new issue' do
+        user1 = User.create!(username: "dev0",
+        role: "developer",
+        email: "d1@d.com",
+        password: "GoHome4",
+        password_confirmation: "GoHome4")
+        
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: user1.email
+        fill_in 'Password', with: user1.password
+        click_on 'Log in'
+        click_on 'Projects'
+        click_on 'Proj23'
+        select project23.title, :from => "title"
+        fill_in 'summary', with: 'Test summary'
+        fill_in 'description', with: 'Test description'
+        fill_in 'status', with: 'Test status'
+        click_on 'Save'
+        expect(page).to have_content 'Test summary'
+        expect(page).to have_content 'Test description'
+        expect(page).to have_content 'Test status'
+    end
+end
